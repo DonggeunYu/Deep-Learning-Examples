ba5cf96be47c31ca4af99e3f2c27aed6cd00559d
Yudonggeun <jkl8124@icloud.com>
2018-08-13 20:02:12 +0900

    Update README.md

--- GAN/MNIST/Model.py
+++ GAN/MNIST/Model.py
@@ -6,117 +6,84 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data/')
 
 train_x = mnist.train.images
-train_y = mnist.train.labels
 
-print('Check: ', train_x.shape, train_y.shape)
-
-total_epochs = 100
+total_epochs = 1000
 batch_size = 100
 learning_rate = 2e-4
 
 
-def generator(z, reuse=False):
+gw1 = tf.Variable(tf.truncated_normal(shape=[128, 256], stddev=0.1))
+gb1 = tf.Variable(tf.truncated_normal(shape=[256], stddev=0.1))
+gw2 = tf.Variable(tf.truncated_normal(shape=[256, 784], stddev=0.1))
+gb2 = tf.Variable(tf.truncated_normal(shape=[784], stddev=0.1))
 
-    if reuse==False:
-        with tf.variable_scope(name_or_scope='Gen') as scope:
-            gw1 = tf.Variable(tf.truncated_normal(shape=[128, 256], stddev=0.1))
-            gb1 = tf.Variable(tf.truncated_normal(shape=[256], stddev=0.1))
-            gw2 = tf.Variable(tf.truncated_normal(shape=[256, 784], stddev=0.1))
-            gb2 = tf.Variable(tf.truncated_normal(shape=[784], stddev=0.1))
-    else:
-        with tf.variable_scope(name_or_scope='Gen', reuse = True) as scope :
-            gw1 = tf.Variable(tf.truncated_normal(shape=[128, 256], stddev=0.1))
-            gb1 = tf.Variable(tf.truncated_normal(shape=[256], stddev=0.1))
-            gw2 = tf.Variable(tf.truncated_normal(shape=[256, 784], stddev=0.1))
-            gb2 = tf.Variable(tf.truncated_normal(shape=[784], stddev=0.1))
 
+def Generator(z):
     hidden = tf.nn.relu(tf.matmul(z, gw1) + gb1)
     output = tf.nn.sigmoid(tf.matmul(hidden, gw2) + gb2)
 
     return output
 
 
-def discriminator(x, reuse=False):
+dw1 = tf.Variable(tf.truncated_normal(shape=[784, 256], stddev=0.1))
+db1 = tf.Variable(tf.truncated_normal(shape=[256], stddev=0.1))
+dw2 = tf.Variable(tf.truncated_normal(shape=[256, 1], stddev=0.1))
+db2 = tf.Variable(tf.truncated_normal(shape=[1], stddev=0.1))
 
-    if reuse==False:
-        with tf.variable_scope(name_or_scope='Dis') as scope :
-            dw1 = tf.Variable(tf.truncated_normal(shape=[784, 256], stddev=0.1))
-            db1 = tf.Variable(tf.truncated_normal(shape=[256], stddev=0.1))
-            dw2 = tf.Variable(tf.truncated_normal(shape=[256, 1], stddev=0.1))
-            db2 = tf.Variable(tf.truncated_normal(shape=[1], stddev=0.1))
-    else:
-        with tf.variable_scope(name_or_scope='Dis', reuse=True) as scope:
-            dw1 = tf.Variable(tf.truncated_normal(shape=[784, 256], stddev=0.1))
-            db1 = tf.Variable(tf.truncated_normal(shape=[256], stddev=0.1))
-            dw2 = tf.Variable(tf.truncated_normal(shape=[256, 1], stddev=0.1))
-            db2 = tf.Variable(tf.truncated_normal(shape=[1], stddev=0.1))
 
+
+def Discrimination(x):
     hidden = tf.nn.relu(tf.matmul(x, dw1) + db1)
     output = tf.nn.sigmoid(tf.matmul(hidden, dw2) + db2)
+
     return output
 
 
-def random_noise(batch_size):
+def Random_noise(batch_size):
     return np.random.normal(size=[batch_size, 128])
 
 
-g = tf.Graph()
+X = tf.placeholder(tf.float32, shape=[None, 784])
+Z = tf.placeholder(tf.float32, shape=[None, 128])
 
-with g.as_default():
+fake_x = Generator(Z)
 
-    X = tf.placeholder(tf.float32, [None, 784])
-    Z = tf.placeholder(tf.float32, [None, 128])
+result_of_fake = Discrimination(fake_x)
+result_of_real = Discrimination(X)
 
-    fake_x = generator(Z)
+g_loss = -tf.reduce_mean(tf.log(result_of_fake))
+d_loss = -tf.reduce_mean(tf.log(result_of_real) + tf.log(1- result_of_fake))
 
-    result_of_fake = discriminator(fake_x)
-    result_of_real = discriminator(X, True)
-
-    g_loss = tf.reduce_mean(tf.log(result_of_fake))
-    d_loss = tf.reduce_mean(tf.log(result_of_real) + tf.log(1 - result_of_fake))
-
-    t_vars = tf.trainable_variables()
-
-    g_vars = [var for var in t_vars if 'Gen' in var.name]
-    d_vars = [var for var in t_vars if 'Dis' in var.name]
-
-    optimizer = tf.train.AdamOptimizer(learning_rate)
-
-    g_train = optimizer.minimize(-g_loss, var_list=g_vars)
-    d_train = optimizer.minimize(-d_loss, var_list=d_vars)
+g_train = tf.train.AdamOptimizer(learning_rate).minimize(g_loss, var_list=[gw1, gb1, gw2, gb2])
+d_train = tf.train.AdamOptimizer(learning_rate).minimize(d_loss, var_list=[dw1, db1, dw2, db2])
 
 
-with tf.Session(graph=g) as sess:
+with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-
     total_batchs = int(train_x.shape[0] / batch_size)
 
     for epoch in range(total_epochs):
 
         for batch in range(total_batchs):
-            batch_x = train_x[batch * batch_size: (batch+1) * batch_size]  # [batch_size , 784]
-            batch_y = train_y[batch * batch_size: (batch+1) * batch_size]  # [batch_size,]
-            noise = random_noise(batch_size)  # [batch_size, 128]
+            batch_x = train_x[batch * batch_size: (batch + 1) * batch_size]
+            noise = Random_noise(batch_size)
 
             sess.run(g_train, feed_dict={Z: noise})
             sess.run(d_train, feed_dict={X: batch_x, Z: noise})
 
             gl, dl = sess.run([g_loss, d_loss], feed_dict={X: batch_x, Z: noise})
 
-        if (epoch+1) % 1 == 0 or epoch == 1:
-            print('=======Epoch: ', epoch, '=======================================')
-            print('Generator: ', gl)
-            print('Discrimination: ', dl)
+        print('=======Epoch: ', epoch, '=======================================')
+        print('Generator: ', gl)
+        print('Discrimination: ', dl)
 
-        if epoch == 0 or (epoch + 1) % 1 == 0:
-            sample_noise = random_noise(10)
-
-            generated = sess.run(fake_x , feed_dict = { Z : sample_noise})
-
+        if epoch % 10 == 0:
+            sample_noise = Random_noise(10)
+            generated = sess.run(fake_x, feed_dict={Z: sample_noise})
             fig, ax = plt.subplots(1, 10, figsize=(10, 1))
-            for i in range(10) :
+            for i in range(10):
                 ax[i].set_axis_off()
-                ax[i].imshow( np.reshape( generated[i], (28, 28)) )
+                ax[i].imshow(np.reshape(generated[i], (28, 28)))
 
             plt.savefig('goblin-gan-generated/{}.png'.format(str(epoch).zfill(3)), bbox_inches='tight')
             plt.close(fig)
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Deep-Learning-Projects
+# Deep Learning Projects
+
+이 Repository는 Deep Learning과 관련된 프로젝트를 학습시키며 정리한 내용을 저장한 곳이다.
+
+
+
+<br/>
+
+
+
+## Category List
+
+Alphabet ascending.
+
+##### 1. [CNN (Convolutional Neural Network) (2 Projects)](./CNN)
+
+##### 2. [GAN (Generative Adversarial Nets) (1 Projects)](./GAN)
+
+
+
+<br/>
+
+
+
+## CNN (Convolutional Neural Network)
+
+Alphabet ascending.
+
+##### 1. [Face recognition with use my image](./CNN/Face-recognition-with-use-my-image)
+
+##### 2. [MNIST](./CNN/MNIST)
+
+
+
+<br/>
+
+## GAN (Generative Adversarial Nets)
+
+Alphabet ascending.
+
+##### 1. [MNIST](./GAN/MNIST)
